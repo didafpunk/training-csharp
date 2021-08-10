7ecadbbbf349ee89719fcb26c3409ae9b011705b
Nordinne Ben Haddou <nordinnebenhaddou@hotmail.com>
2021-08-10 17:48:51 +0200

    generateur de mot de passe

---
+++ generateur_de_mot_de_passe/generateur_de_mot_de_passe/Program.cs
@@ -0,0 +1,64 @@
+﻿using formationcs;
+using System;
+
+
+namespace generateur_de_mot_de_passe
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            const int NB_MOT_DE_PASSSE = 10;
+
+            int longueurMotDePasse = outils.DemanderNombrePositiveNonNul("Longueur du mot de passe : ");
+            Console.WriteLine();
+
+            Random rand = new Random();
+            String minuscule = "abcdefghijklmnopqrstuvwxyz";
+            String majuscule = minuscule.ToUpper();
+            String chiffre = "0123456789";
+            String caractereSpeciaux = "#+-!@$";
+            String alphabet ;
+            String motDePasse = "";
+            int choixAlphabet = outils.DemanderNombreEntre("Veuillez choisir un mot de passe :\n" +
+                 "1- uniqueement minuscule \n" +
+                 "2- minusculae et majascule \n" +
+                 "3- characte et chiffre \n" +
+                 "4- la totale \n" +
+                "Faites Votre choix : ", 1, 4);
+            Console.WriteLine();
+
+            if (choixAlphabet == 1)
+                alphabet = minuscule;
+           
+            else if (choixAlphabet == 2)
+                alphabet = minuscule + majuscule;
+            else if (choixAlphabet == 3)
+                alphabet = minuscule + majuscule + chiffre;
+            else
+                alphabet = minuscule + majuscule + chiffre + caractereSpeciaux;
+
+            int l = alphabet.Length;
+            int index = rand.Next(0, l);
+            for (int y = 0; y < NB_MOT_DE_PASSSE; y++)
+            {
+                motDePasse = "";
+                for (int i = 0; i < longueurMotDePasse; i++)
+                {
+
+                    index = rand.Next(0, l);
+                    motDePasse += alphabet[index];
+                
+
+                }
+            Console.WriteLine($"Mot de passe :  {motDePasse}");
+
+            }
+
+
+        }
+
+    }
+}
---
+++ generateur_de_mot_de_passe/generateur_de_mot_de_passe/outils.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formationcs
+
+
+{
+    class outils
+    {
+        public static int DemanderNombrePositiveNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue,"Erreur le nombre doit etre positf et non nul");
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max, string messagePerso=null)
+        {
+            int reponse = DemanderNombre(question);
+
+            if (reponse >= min && reponse <= max )
+            {
+                return reponse;
+            }
+
+            if (messagePerso == null)
+            {
+                Console.WriteLine($"Erreur: le nombre doit etre entre {min} et {max} ");
+
+            }
+            else
+            {
+                Console.WriteLine(messagePerso);
+            }
+
+            Console.WriteLine();
+            
+            return DemanderNombreEntre(question, min, max);
+
+        }
+
+        public static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                string reponse_str = Console.ReadLine();
+
+                try
+                {
+                    int reponseInt = int.Parse(reponse_str);
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("Erruer entrez un nombre numerique");
+
+                    Console.WriteLine();
+                }
+
+            }
+
+        }
+
+
+
+    }
+}
