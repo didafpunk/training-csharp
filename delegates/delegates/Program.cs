3f81d06c5dae5909b8ed61a404c9b78fa87462a0
didafpunk <nordinnebenhaddou@hotmail.com>
2021-08-12 00:31:39 +0200

    end of delegates

--- delegates/delegates/Program.cs
+++ delegates/delegates/Program.cs
@@ -1,29 +1,81 @@
 ﻿using System;
+using System.Linq;
 
 namespace delegates
 {
     class Program
     {
+        public delegate string ValidationChaine(string s);
+
         static void Main(string[] args)
         {
-            //demander le nom de l'utilisateur;
-            //demander le numero de telephone de l'utilisateur
 
-            string nom = Demander("quel est ton nom :");
-            string numero = Demander("quel est votre numero :");
+
+            string nom = DemanderChaineUtilisateur("quel est ton nom :",CheckValidationDuNom);
+
+            //max 10 chiffres
+            string numero = DemanderChaineUtilisateur("quel est votre numero :", CheckValidationDuNumero);
 
             Console.WriteLine();
-            Console.WriteLine("Bonjour "+ nom+ ", vous etes joignable au numero : "+numero);
+            Console.WriteLine("Bonjour " + nom + ", vous etes joignable au numero : " + numero);
+
+
 
         }
-        static string Demander(string message)
+        static string DemanderChaineUtilisateur(string message, ValidationChaine fonctionValidation = null)
         {
 
             Console.Write(message);
             string reponse = Console.ReadLine();
+            if (fonctionValidation != null)
+            {
+                string erreur = fonctionValidation(reponse);
+                if (erreur != null)
+                {
+                    Console.WriteLine("ERREUR :" + erreur);
+                    return DemanderChaineUtilisateur(message,fonctionValidation);
+                }
+
+            }
             return reponse;
+
         }
-        
+
+        static string CheckValidationDuNom(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+
+                return "ne doit pas etre vide";
+            }
+
+
+            if (s.Any(char.IsDigit))
+            {
+
+                return "ne peut pas contenir de chiffre";
+            }
+            return null;
+        }
+        static string CheckValidationDuNumero(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+
+                return "le numero de tel ne doit pas etre vide";
+            }
+            if (s.Length != 10)
+            {
+              return "le nmero de tel doit contenir 10 chiffres ";
+            }
+
+            if (!s.All(char.IsDigit))
+            {
+
+                return "le numero de tel ne doit continir que des chiffre";
+            }
+            return null;
+        }
 
     }
 }
