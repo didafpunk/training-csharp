b1b382d84fb1659cb720e0541534fbfb57d2e5eb
didafpunk <nordinnebenhaddou@hotmail.com>
2021-08-13 23:23:33 +0200

    asyncDelegate
    Funtion asyncron delegate

---
+++ AsyncDelegate/AsyncDelegate/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace AsyncDelegate
+{
+    class Program
+    {
+
+        static bool donwloading = true;
+
+        static void Main(string[] args)
+        {
+
+
+
+            string filename = "bateau.jpg";
+            string url = "https://codeavecjonathan.com/res/bateau.jpg";
+            var webClient = new WebClient();
+
+            webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+
+            Console.Write("Downloading.");
+            webClient.DownloadFileAsync(new Uri(url), filename);
+
+
+            while (donwloading)
+            {
+
+                Thread.Sleep(500);
+                if (donwloading)
+
+                {
+                    Console.Write(".");
+                }
+            }
+
+
+
+
+        }
+
+        private static void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            Console.WriteLine("Downloading finished");
+            donwloading = false;
+        }
+    }
+}
